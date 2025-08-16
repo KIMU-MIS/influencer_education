d65d61b97b53bb2d3dacd8323f97cb851bb90204
2001014 <2001014@s.asojuku.ac.jp>
2025-08-16 15:59:17 +0900

    style(user): fix indentation in User\ArticleController

--- app/Http/Controllers/User/ArticleController.php
+++ app/Http/Controllers/User/ArticleController.php
@@ -3,22 +3,19 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\Article;
 
 class ArticleController extends Controller
 {
-    /**
-     * お知らせ詳細表示
-     */
-public function show($id)
-{
-    $article = Article::findOrFail($id);
-    return view('user.article_detail', compact('article'));
-}
     public function index()
-{
-    $articles = Article::orderBy('published_date', 'desc')->get();
-    return view('user.article_list', compact('articles'));
-}
+    {
+        $articles = Article::orderBy('published_date', 'desc')->get();
+        return view('user.article_list', compact('articles'));
+    }
+
+    public function show($id)
+    {
+        $article = Article::findOrFail($id);
+        return view('user.article_detail', compact('article'));
+    }
 }
